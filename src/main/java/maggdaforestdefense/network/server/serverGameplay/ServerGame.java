5c39a008053f73759d695af6360b5ff5da921e18
DaJaBeCoding <38948265+DaJaBeCoding@users.noreply.github.com>
2020-11-25 23:16:15 +0100

    hab essenz vergessen lal

--- src/main/java/maggdaforestdefense/network/server/serverGameplay/ServerGame.java
+++ src/main/java/maggdaforestdefense/network/server/serverGameplay/ServerGame.java
@@ -40,7 +40,7 @@ import maggdaforestdefense.util.Waiter;
  */
 public class ServerGame extends Thread {
 
-    public final static int START_COINS = 100, START_ESSENCE = 20;
+    public final static int START_COINS = 100, START_ESSENCE = 8;
     
     // MANAGEMENT
     private final String name;

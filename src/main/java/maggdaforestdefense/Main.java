12e7075f5ba54df2934a22caced5e4e69322dee3
MinorTom <me@minortom.net>
2020-11-25 17:42:45 +0100

    Revert aa79f4a5 DaJaBeCoding <38948265+dajabecoding@users.noreply.github.com> on 11.10.20 at 17:02 Recreate Main.java

---
+++ src/main/java/maggdaforestdefense/Main.java
@@ -0,0 +1,11 @@
+package maggdaforestdefense;
+
+import javafx.scene.text.Font;
+import maggdaforestdefense.storage.Logger;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MaggdaForestDefense.main(args);
+    }
+}

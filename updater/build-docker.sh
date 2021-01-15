137db96eaa836e5afcdfecf44803c681f72223c3
MinorTom <me@minortom.net>
2021-01-15 20:34:22 +0100

    Fix build?

--- updater/build-docker.sh
+++ updater/build-docker.sh
@@ -13,19 +13,20 @@ echo "# Building"
 mkdir updater/tmp
 
 echo 'Copying stuff'
-cp build/distributions/ForestDefense-app.zip updater/tmp/ForestDefense-app.zip
+#cp build/distributions/ForestDefense-app.zip updater/tmp/ForestDefense-app.zip
+cp build/libs/ForestDefense-app.jar updater/ForestDefense/ForestDefense.jar
 # shellcheck disable=SC2164
-cd updater/tmp/
-unzip ForestDefense-app.zip
+#cd updater/tmp/
+#unzip ForestDefense-app.zip
 # shellcheck disable=SC2103
-cd ..
-cp -r tmp/ForestDefense-app/lib ForestDefense
-cp -r tmp/ForestDefense-app/bin ForestDefense
+#cd ..
+#cp -r tmp/ForestDefense-app/lib ForestDefense
+#cp -r tmp/ForestDefense-app/bin ForestDefense
 cp ../OPENSOURCELICENSES.md ./ForestDefense/OPENSOURCELICENSES.md
 #cp tmp/ForestDefense-app/lib/ForestDefense-app.jar ForestDefense/ForestDefense-app.jar
 
-echo '# Generating digest'
-java -classpath getdown-core-1.8.6.jar com.threerings.getdown.tools.Digester ForestDefense
+echo '# Skipping generating digest'
+# java -classpath getdown-core-1.8.6.jar com.threerings.getdown.tools.Digester ForestDefense
 
 echo '# Generating open source licenses'
 cp ../OPENSOURCELICENSES.md ./ForestDefense/OPENSOURCELICENSES.md

eb37c8d0f0447b6a3908a4798b1038b41e9b88ff
MinorTom <me@minortom.net>
2021-01-15 20:40:16 +0100

    Fix build?

--- updater/build-docker.sh
+++ updater/build-docker.sh
@@ -11,32 +11,35 @@ echo "# Building"
 ./gradlew build --no-daemon
 
 mkdir updater/tmp
+mkdir updater/ForestDefense2
 
 echo 'Copying stuff'
 #cp build/distributions/ForestDefense-app.zip updater/tmp/ForestDefense-app.zip
-cp build/libs/ForestDefense-app.jar updater/ForestDefense/ForestDefense.jar
+cp build/libs/ForestDefense-app.jar updater/ForestDefense2/ForestDefense.jar
 # shellcheck disable=SC2164
+cd updater/
 #cd updater/tmp/
 #unzip ForestDefense-app.zip
 # shellcheck disable=SC2103
 #cd ..
 #cp -r tmp/ForestDefense-app/lib ForestDefense
 #cp -r tmp/ForestDefense-app/bin ForestDefense
-cp ../OPENSOURCELICENSES.md ./ForestDefense/OPENSOURCELICENSES.md
 #cp tmp/ForestDefense-app/lib/ForestDefense-app.jar ForestDefense/ForestDefense-app.jar
 
 echo '# Skipping generating digest'
 # java -classpath getdown-core-1.8.6.jar com.threerings.getdown.tools.Digester ForestDefense
 
 echo '# Generating open source licenses'
-cp ../OPENSOURCELICENSES.md ./ForestDefense/OPENSOURCELICENSES.md
+cp ../OPENSOURCELICENSES.md ./ForestDefense2/OPENSOURCELICENSES.md
 pandoc ./../OPENSOURCELICENSES.md -f markdown -t html -s --metadata title="OPEN SOURCE LICENSES" --toc -H ./opensourcelicenses/style.html -A ./opensourcelicenses/footer.html -o ./web/opensourcelicenses.html
 
-echo "# Generating update"
+echo "# Skipping generating update"
 # shellcheck disable=SC2164
-cp -r ForestDefense ./web/updater/ForestDefense
+#cp -r ForestDefense ./web/updater/ForestDefense
 
 echo "# Generating ienokihpkg update"
+
+mv ForestDefense2 ForestDefense
 
 wget https://ienokih.pages.minortom.net/ienokihpkg-lockfilegenerator/lockfilegenerator
 cd ForestDefense || exit

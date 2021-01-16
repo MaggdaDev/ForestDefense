a670245f40581f8667d0566c368f1578c739a5ff
MinorTom <me@minortom.net>
2021-01-16 14:53:51 +0100

    2 Gradle builds

--- updater/build-docker.sh
+++ updater/build-docker.sh
@@ -7,7 +7,7 @@ echo "# Setting version"
 rm src/main/resources/maggdaforestdefense/config/version.txt
 date +"Version %y%m%d-%H%M%S (alpha)" > src/main/resources/maggdaforestdefense/config/version.txt
 
-echo "# Building"
+echo "# Building server"
 ./gradlew build --no-daemon
 
 mkdir updater/tmp
@@ -15,7 +15,6 @@ mkdir updater/ForestDefense2
 
 echo 'Copying stuff'
 cp build/distributions/ForestDefense-app.zip updater/tmp/ForestDefense-app.zip
-cp build/libs/ForestDefense-app.jar updater/ForestDefense2/ForestDefense.jar
 # shellcheck disable=SC2164
 cd updater/
 cd tmp/
@@ -26,21 +25,32 @@ cp -r tmp/ForestDefense-app/lib ForestDefense
 cp -r tmp/ForestDefense-app/bin ForestDefense
 #cp tmp/ForestDefense-app/lib/ForestDefense-app.jar ForestDefense/ForestDefense-app.jar
 
-echo '# Skipping generating digest'
+#echo '# Generating digest'
 # java -classpath getdown-core-1.8.6.jar com.threerings.getdown.tools.Digester ForestDefense
 
-echo '# Generating open source licenses'
-cp ../OPENSOURCELICENSES.md ./ForestDefense2/OPENSOURCELICENSES.md
-pandoc ./../OPENSOURCELICENSES.md -f markdown -t html -s --metadata title="OPEN SOURCE LICENSES" --toc -H ./opensourcelicenses/style.html -A ./opensourcelicenses/footer.html -o ./web/opensourcelicenses.html
+#echo '# Generating open source licenses'
+#cp ../OPENSOURCELICENSES.md ./ForestDefense2/OPENSOURCELICENSES.md
+#pandoc ./../OPENSOURCELICENSES.md -f markdown -t html -s --metadata title="OPEN SOURCE LICENSES" --toc -H ./opensourcelicenses/style.html -A ./opensourcelicenses/footer.html -o ./web/opensourcelicenses.html
 
-echo "# Skipping generating update"
+#echo "#Generating update"
 # shellcheck disable=SC2164
-cp -r ForestDefense ./web/updater/ForestDefense
+#cp -r ForestDefense ./web/updater/ForestDefense
 
 echo "# Generating ienokihpkg update"
 
+
+
 rm -rf ForestDefense
 mkdir ForestDefense
+
+cd ..
+
+rm build.gradle
+./gradlew build --no-daemon
+
+cp build/libs/ForestDefense-app.jar updater/ForestDefense2/ForestDefense.jar
+
+cd updater
 
 mv ForestDefense2/* ForestDefense
 

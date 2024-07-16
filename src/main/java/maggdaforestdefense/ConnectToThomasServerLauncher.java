40f19a819438235e96549bb67f828e81f6c75b6c
MaggdaDev <davidbetko05@gmail.com>
2024-07-16 20:33:29 +0200

    Added ConnectToThomasServerLauncher for launching and connecting to Thomas server

---
+++ src/main/java/maggdaforestdefense/ConnectToThomasServerLauncher.java
@@ -0,0 +1,7 @@
+package maggdaforestdefense;
+
+public class ConnectToThomasServerLauncher {
+    public static void main(String[] args) {
+        MaggdaForestDefense.main(new String[]{"--ip=ws://minortom.net:27756"});
+    }
+}
